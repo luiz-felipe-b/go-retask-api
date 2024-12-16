66502b634c1ca766e0b6e809a3e03e0a9d3889e7
luiz-felipe-b <lfbalaminute@hotmail.com>
2024-12-16 00:08:56 -0300

    create(arrays): sum all and sum all tails functions

---
+++ arrays/sum_all.go
@@ -0,0 +1,20 @@
+package arrays
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return
+}
